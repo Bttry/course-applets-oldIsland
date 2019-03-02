4fd197540ad463c6894bd7b95d58d349a92c84a6
StriveNuby <1477970338@qq.com>
2019-03-02 16:30:30 +0800

    no-message

---
+++ pages/book/book.js
@@ -0,0 +1,122 @@
+// pages/book/book.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    books: [
+      {
+        author: 'MICK',
+        fav_nums: 9,
+        _id: 9,
+        image: 'http://file.ituring.com.cn/SmallCover/1712477631b07b9f5895',
+        like_status: 0,
+        title: 'SQL基础教程（第2版）'
+      },
+      {
+        author: 'MICK',
+        fav_nums: 9,
+        _id: 9,
+        image: 'http://file.ituring.com.cn/SmallCover/1712477631b07b9f5895',
+        like_status: 0,
+        title: 'SQL基础教程（第2版）'
+      },
+      {
+        author: 'MICK',
+        fav_nums: 9,
+        _id: 9,
+        image: 'http://file.ituring.com.cn/SmallCover/1712477631b07b9f5895',
+        like_status: 0,
+        title: 'SQL基础教程（第2版）'
+      },
+      {
+        author: 'MICK',
+        fav_nums: 9,
+        _id: 9,
+        image: 'http://file.ituring.com.cn/SmallCover/1712477631b07b9f5895',
+        like_status: 0,
+        title: 'SQL基础教程（第2版）'
+      },
+      {
+        author: 'MICK',
+        fav_nums: 9,
+        _id: 9,
+        image: 'http://file.ituring.com.cn/SmallCover/1712477631b07b9f5895',
+        like_status: 0,
+        title: 'SQL基础教程（第2版）'
+      },
+      {
+        author: 'MICK',
+        fav_nums: 9,
+        _id: 9,
+        image: 'http://file.ituring.com.cn/SmallCover/1712477631b07b9f5895',
+        like_status: 0,
+        title: 'SQL基础教程（第2版）'
+      },
+      {
+        author: 'MICK',
+        fav_nums: 9,
+        _id: 9,
+        image: 'http://file.ituring.com.cn/SmallCover/1712477631b07b9f5895',
+        like_status: 0,
+        title: 'SQL基础教程（第2版）'
+      },
+      {
+        author: 'MICK',
+        fav_nums: 9,
+        _id: 9,
+        image: 'http://file.ituring.com.cn/SmallCover/1712477631b07b9f5895',
+        like_status: 0,
+        title: 'SQL基础教程（第2版）'
+      },
+      {
+        author: 'MICK',
+        fav_nums: 9,
+        _id: 9,
+        image: 'http://file.ituring.com.cn/SmallCover/1712477631b07b9f5895',
+        like_status: 0,
+        title: 'SQL基础教程（第2版）'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {},
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {}
+})
